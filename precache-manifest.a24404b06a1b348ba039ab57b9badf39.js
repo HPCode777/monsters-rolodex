c56ba2b67e86d2f2f388cd77a10a7b64e2ed4840
HPCode777 <hpcode777@gmail.com>
2020-04-10 19:40:08 +0700

    Updates

---
+++ precache-manifest.a24404b06a1b348ba039ab57b9badf39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cd28c0cf5ddafa3a67e915f400af5ac",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c0d3abd81eb20d48f417",
+    "url": "/monsters-rolodex/static/css/main.4c3ca15a.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0d3abd81eb20d48f417",
+    "url": "/monsters-rolodex/static/js/main.22b9db81.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
